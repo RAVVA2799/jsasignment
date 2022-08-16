d312e5f7a70fd9d9f44c7a42d056f1222498ddb9
Pavan raju <pavankumarraju7@gmail.com>
2022-08-17 01:10:19 +0530

    asthird

--- a3.js
+++ a3.js
@@ -2,4 +2,4 @@ var input=7;
 var output1= ++input + ++input + ++input;
 var output2= input++ + input++ + input++;
 var output3= input++ + ++input + input++;
-console.log(output1+''+output2+''+output3);
+console.log(output1+' '+output2+' '+output3);
