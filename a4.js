a30f90c0242474251cedaf3d2897e0db4f2aed95
Pavan raju <pavankumarraju7@gmail.com>
2022-08-17 01:01:54 +0530

    yes

---
+++ a4.js
@@ -0,0 +1,45 @@
+//q1 asg
+var x=0;
+x=x+1;         // option 2 is correct
+console.log(x);
+
+//q2 asg
+
+varx=1;
+x=x-1;    //option 2 is correct
+console.log(x);
+
+
+
+
+
+
+//asg q3
+var num=100;
+num+=5*10/2;
+console.log(num);
+
+
+
+var num=100;    //asignment q4
+num*=2*10/5;
+console.log(num);
+
+
+//asg q5
+
+var num=100;
+num/=2*8-15;
+console.log(num);
+
+
+
+//comparision operator 
+
+console.log(-4+5- +"" + -2);
+
+//q2 asg correct answer is -1
+
+
+//q1 asg correct ans is 6.00
+
